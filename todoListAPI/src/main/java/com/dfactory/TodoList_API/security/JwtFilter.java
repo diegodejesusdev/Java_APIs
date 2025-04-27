1e4e1272537d0292e689e9c093fe3dcb3c3c1541
Diego Dejesus <diegodejesuspersonal@gmail.com>
2025-04-27 18:51:45 -0500

    Se añadió JwtFilter para filtrar la autenticación del usuario

--- todoListAPI/src/main/java/com/dfactory/TodoList_API/security/JwtFilter.java
+++ todoListAPI/src/main/java/com/dfactory/TodoList_API/security/JwtFilter.java
@@ -1,11 +1,15 @@
 package com.dfactory.TodoList_API.security;
 
+import com.dfactory.TodoList_API.model.UserEntity;
 import com.dfactory.TodoList_API.repository.UserRepository;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
@@ -20,6 +24,23 @@ public class JwtFilter extends OncePerRequestFilter {
 
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        String authHeader = request.getHeader("Authorization");
+        if(authHeader != null && !authHeader.isEmpty()){
+            String token = authHeader;
+            String email = jwtUtil.extractUsername(token);
 
+            UserEntity user = userRepository.findByEmail(email).orElse(null);
+
+            if(user != null && jwtUtil.isTokenValid(token, user)){
+                UsernamePasswordAuthenticationToken authenticationToken = new UsernamePasswordAuthenticationToken(
+                        user.getEmail(),
+                        null,
+                        java.util.Collections.emptyList()
+                );
+                authenticationToken.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
+                SecurityContextHolder.getContext().setAuthentication(authenticationToken);
+            }
+        }
+        filterChain.doFilter(request, response);
     }
 }
