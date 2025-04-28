80c1705bcd51c2e7a4dc5c9afa72283323cbc14d
Diego Dejesus <diegodejesuspersonal@gmail.com>
2025-04-27 20:31:51 -0500

    Se añadió el DTO de TodoResponse

---
+++ todoListAPI/src/main/java/com/dfactory/TodoList_API/dto/TodoResponse.java
@@ -0,0 +1,14 @@
+package com.dfactory.TodoList_API.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class TodoResponse {
+    private Long id;
+    private String title;
+    private String description;
+}
